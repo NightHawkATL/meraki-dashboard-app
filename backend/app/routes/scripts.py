5c4c2773d6bd2e89c9dbef585032e5521e268b65
NightHawk-ATL <1+nighthawk-atl@noreply@myservweb.com>
2026-07-08 20:02:44 -0400

    Update backend/app/routes/scripts.py

--- backend/app/routes/scripts.py
+++ backend/app/routes/scripts.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Form, Request
+from fastapi import APIRouter, Depends, Form, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
